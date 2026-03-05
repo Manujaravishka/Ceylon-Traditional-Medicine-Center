56e9172bc4739ada927d90980f39d95a01dd9808
Manujaravishka <manujaravishka2004@gmail.com>
2026-03-05 22:17:33 +0530

    First Commit (create Spring Boot Project)

---
+++ src/main/java/org/example/ceylontraditionalmedicinecenter/CeylonTraditionalMedicineCenterApplication.java
@@ -0,0 +1,13 @@
+package org.example.ceylontraditionalmedicinecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonTraditionalMedicineCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeylonTraditionalMedicineCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ceylontraditionalmedicinecenter/CeylonTraditionalMedicineCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ceylontraditionalmedicinecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonTraditionalMedicineCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
