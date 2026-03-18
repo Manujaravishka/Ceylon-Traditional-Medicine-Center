7de40f15406cbba81321620f48d2823febd7d111
Manujaravishka <manujaravishka2004@gmail.com>
2026-03-18 19:37:16 +0530

    fifth Commit (dto / entity )

--- src/main/java/org/example/ceylontraditionalmedicinecenter/entity/Role.java
+++ src/main/java/org/example/ceylontraditionalmedicinecenter/entity/Role.java
@@ -1,4 +1,5 @@
 package org.example.ceylontraditionalmedicinecenter.entity;
 
 public enum Role {
+//    ADMIN,USER
 }
--- src/main/java/org/example/ceylontraditionalmedicinecenter/entity/User.java
+++ src/main/java/org/example/ceylontraditionalmedicinecenter/entity/User.java
@@ -1,4 +1,28 @@
 package org.example.ceylontraditionalmedicinecenter.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uid;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String contact;
+    private String password;
+    private String role;
 }
